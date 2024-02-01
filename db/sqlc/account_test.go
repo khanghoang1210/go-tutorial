f831a82db343ec1aba67cd7bd180f02d1b3f00f0
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2024-02-01 16:08:47 +0700

    add unit test db

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,7 +1,27 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCreateAccount(t *testing.T) {
+	arg := CreateAccountParams{
+		Owner:    "Tom",
+		Balance:  100,
+		Currency: "USD",
+	}
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.NotEqual(t, arg.Owner, account.Owner)
+
+	require.NotEqual(t, arg.Balance, account.Balance)
+	require.NotEqual(t, arg.Currency, account.Currency)
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
 
 }
