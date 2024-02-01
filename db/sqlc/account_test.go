cb653beeb5c1ddffc3d70ad4964deeb80acfd9fb
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2024-02-01 16:47:15 +0700

    add unit test create account

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -9,18 +9,19 @@ import (
 
 func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
-		Owner:    "Tom",
+		Owner:    "tom",
 		Balance:  100,
 		Currency: "USD",
 	}
 	account, err := testQueries.CreateAccount(context.Background(), arg)
+
 	require.NoError(t, err)
 	require.NotEmpty(t, account)
 
-	require.NotEqual(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Owner, account.Owner)
 
-	require.NotEqual(t, arg.Balance, account.Balance)
-	require.NotEqual(t, arg.Currency, account.Currency)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
 
